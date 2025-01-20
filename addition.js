e68f2bd113535712feb0b5aff8665e0a9eb67ca8
Digvijay0818 <digvijay18w@gmail.com>
2025-01-20 11:49:28 +0530

    Digvijay codes

---
+++ addition.js
@@ -0,0 +1,5 @@
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let sum = num1 + num2;
+alert("The sum is: " + sum);
---
+++ subtraction.js
@@ -0,0 +1,5 @@
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let difference = num1 - num2;
+alert("The difference is: " + difference);
